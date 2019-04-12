3cd916db6b734a5714b4f9c4ebbd61d08a199128
Scott Lehman <scottsgllehman@gmail.com>
2019-04-11 21:08:44 -0600

    updated database and results

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,28 +3,24 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/tweets", function(req, res) {
-    db.Tweet.findAll({
-      where: {
-        trend: req.body
-      }
-    }).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+    if (req.query.trend) {
+      db.Tweet.findAll({
+        where: {
+          trend: req.query.trend
+        }
+      }).then(function(dbExamples) {
+        res.json(dbExamples);
+      });
+    } else {
+      res.status(400);
+      res.send("None shall pass");
+    }
   });
 
   // Create a new example
   app.post("/api/tweets", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.Tweet.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
-
-  // Delete an example by id
-
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-
-
+};
