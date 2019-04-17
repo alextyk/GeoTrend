7233ebb3d041e3bdab69341188ee63e1aa5fa8a7
Alex Tyk <alex.tyk@hootsuite.com>
2019-04-16 18:13:19 -0600

    2 files

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,12 @@
 // bypassing CORS
-
-$(document).ready(function() {
-    $("#tweets").hide();
-    $("#results").hide();
-});
-
 jQuery.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
     }
 });
+$(document).ready(function () {
+    $("#getrouteresults").hide();
+})
 
 // on click that grabs in the input from the text box whex`xn submit is pressed
 $("#searchBtn").on("click", function () {
@@ -68,81 +65,91 @@ function get_trending_topics(woeid) {
         var container = document.getElementById("results");
         for (var i = 0; i < 20; i++) {
             container.innerHTML +=
-                <div class="trends" data-value="${response[0].trends[i].name}"><a href="#">${response[0].trends[i].name}</a></div>;
+                `<div class="trends" data-value="${response[0].trends[i].name}"><a href="#">${response[0].trends[i].name}</a></div>`;
         }
-        $("#results").fadeIn(500);
     });
 }
 
-$("div").on("click", "div.trends", function () {
+$("div").on("click", "div.trends", function() {
 
     $("#tweets").empty();
-    $("#tweets").fadeIn(500);
-
+  
     function getTweets(trend) {
 
         console.log(trend.replace('#', ''));
-
-        $.ajax({
-            url: "https://api.twitter.com/1.1/search/tweets.json?q=" + trend.replace('#', ''),
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAKH%2F5gAAAAAAN9kwQVJuDJk%2Fm1KHHjEugsyFn7c%3DDzSgSVav4KccD3cvoIwEG5lpjoMTjCJVVartb5jfmICvzupeme"
-            }
-            //parse and store responses
-        }).then(function (response) {
-            console.log(response.statuses[0]);
-
-
-            var container = document.getElementById("tweets");
-            for (var i = 0; i < 20; i++) {
-                container.innerHTML +=
-                    `<div class="rawtweets" data-value="${response.statuses[i].text}" data-value1="${response.statuses[i].user.screen_name}" data-value2="${response.statuses[i].retweet_count}" data-value3="${response.statuses[i].favorite_count}" data-value4="${response.statuses[i].user.location}" data-value5="${response.statuses[i].user.name}" data-value6="${trend}" data-value7="${response.statuses[i].user.followers_count}"><a href="#">${response.statuses[i].text}</a></div><br>`;
-
-            }
-        });
+      
+      $.ajax({
+      url: "https://api.twitter.com/1.1/search/tweets.json?q=" + trend.replace('#', ''),
+      method: "GET",
+      headers: {
+          "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAKH%2F5gAAAAAAN9kwQVJuDJk%2Fm1KHHjEugsyFn7c%3DDzSgSVav4KccD3cvoIwEG5lpjoMTjCJVVartb5jfmICvzupeme"
+      }
+      //parse and store responses
+      }).then(function(response) {
+          console.log(response.statuses[0]);
+          
+    
+          var container = document.getElementById("tweets");
+          for (var i = 0; i < 20; i++) {
+              container.innerHTML +=
+                `<div class="rawtweets" data-value="${response.statuses[i].text}" data-value1="${response.statuses[i].user.screen_name}" data-value2="${response.statuses[i].retweet_count}" data-value3="${response.statuses[i].favorite_count}" data-value4="${response.statuses[i].user.location}" data-value5="${response.statuses[i].user.name}" data-value6="${trend}" data-value7="${response.statuses[i].user.followers_count}"><a href="#">${response.statuses[i].text}</a></div><br>`;
+                
+          }
+      });
     }
 
     getTweets($(this).text());
 
-});
+    });
 
-$("div").on("click", "div.rawtweets", function () {
+    $("div").on("click", "div.rawtweets", function() {
 
-    var tweetData = {
-        text: $(this).text(),
-        username: $(this).attr("data-value1"),
-        followers: $(this).attr("data-value7"),
-        profile: $(this).attr("data-value5"),
-        trend: $(this).attr("data-value6"),
-        location: $(this).attr("data-value4"),
-        retweets: $(this).attr("data-value2"),
-        favorites: $(this).attr("data-value3")
+        var tweetData = {
+            text: $(this).text(),
+            username: $(this).attr("data-value1"),
+            followers: $(this).attr("data-value7"),
+            profile: $(this).attr("data-value5"),
+            trend: $(this).attr("data-value6"),
+            location: $(this).attr("data-value4"),
+            retweets: $(this).attr("data-value2"),
+            favorites: $(this).attr("data-value3")
+            
+        }
+        console.log(tweetData);
 
-    }
-    console.log(tweetData);
+        $.post("/api/tweets", tweetData)
 
-    $.post("/api/tweets", tweetData)
-
-        .then(function (data) {
+        .then(function(data) {
             alert("tweet has been posted!")
             console.log(data);
-
+  
+          });
         });
-});
+    $("#searchBtndb").on("click", function () {
+        var trend = $(".searchdb").val().trim();
+        
+        console.log(trend);   
+        $.get("/api/tweets?trend=" + trend)
 
-$("#searchBtndb").on("click", function () {
-    var trend = $(".searchdb").val().trim();
-    console.log(trend);   
-    $.get("/api/tweets?trend=" + trend)
+        .then(function(data){
+            if (data === {}) {
+                console.log("no data!");
+            }
+            console.log(data);
+            for (i = 0; i < data.length; i++) {
+                $("#getrouteresults").append("<strong>Tweet:</strong> " + data[i].text + "<br>");
+                $("#getrouteresults").append("<strong>Username:</strong> " + data[i].username + "<br>");
+                $("#getrouteresults").append("<strong>Profile:</strong> " + data[i].profile + "<br>");
+                $("#getrouteresults").append("<strong>Followers:</strong> " + data[i].followers + "<br>");
+                $("#getrouteresults").append("<strong>Location:</strong> " + data[i].location + "<br>");
+                $("#getrouteresults").append("<strong>Reweets:</strong> " + data[i].retweets + "<br>");
+                $("#getrouteresults").append("<strong>Favorites:</strong> " + data[i].favorites + "<br><br>");
 
-    .then(function(data){
-        if (data === {}) {
-            console.log("no data!");
-        }
-        console.log(data);
-        for (i = 0; i < data.length; i++) {
-            $("#getrouteresults").append(data[i].text);
-        };
+
+
+
+
+            };
+            $("#getrouteresults").fadeIn(200);
+        });
     });
-});
