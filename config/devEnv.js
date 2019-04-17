f87c23a170056e5ef9cc541c8d3ddeeda8095300
Alex Tyk <alex.tyk@hootsuite.com>
2019-04-16 18:43:46 -0600

    restores stashed code

--- config/devEnv.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  dialect: process.env.DIALECT
-};
